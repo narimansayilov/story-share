58d0b9890a36012a96eee233058545479d9ab9fd
Nariman <narimansayilov@gmail.com>
2024-08-10 11:59:30 +0400

    add new findById method

--- src/main/java/com/storyshare/repository/UserRepository.java
+++ src/main/java/com/storyshare/repository/UserRepository.java
@@ -4,8 +4,10 @@ import com.storyshare.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
+import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
     Optional<UserEntity> findByUsername(String username);
     Optional<UserEntity> findByEmail(String email);
+    Optional<UserEntity> findById(UUID id);
 }
