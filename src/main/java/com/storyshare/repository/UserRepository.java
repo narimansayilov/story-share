85821e660b25f7c2773b7719f1930ca3762522c6
Nariman <narimansayilov@gmail.com>
2024-08-11 17:34:02 +0400

    delete method

--- src/main/java/com/storyshare/repository/UserRepository.java
+++ src/main/java/com/storyshare/repository/UserRepository.java
@@ -6,8 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 import java.util.UUID;
 
-public interface UserRepository extends JpaRepository<UserEntity, Long> {
+public interface UserRepository extends JpaRepository<UserEntity, UUID> {
     Optional<UserEntity> findByUsername(String username);
     Optional<UserEntity> findByEmail(String email);
-    Optional<UserEntity> findById(UUID id);
 }
