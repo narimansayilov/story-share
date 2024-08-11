fd83972074ec0347328cd05a603164ac728bbb22
Nariman <narimansayilov@gmail.com>
2024-08-11 17:31:46 +0400

    add tag response

--- src/main/java/com/storyshare/dto/response/TagResponse.java
+++ src/main/java/com/storyshare/dto/response/TagResponse.java
@@ -1,4 +1,14 @@
 package com.storyshare.dto.response;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter
+@Setter
 public class TagResponse {
+    private UUID id;
+    private String name;
+    private Integer storyCount;
 }
