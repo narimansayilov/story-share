50a8bd857812f21b530d2f6ae50dbf41bf57127c
Nariman <narimansayilov@gmail.com>
2024-08-11 17:31:12 +0400

    change repository structure

--- src/main/java/com/storyshare/repository/RoleRepository.java
+++ src/main/java/com/storyshare/repository/RoleRepository.java
@@ -4,7 +4,8 @@ import com.storyshare.entity.RoleEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
+import java.util.UUID;
 
-public interface RoleRepository extends JpaRepository<RoleEntity, Long> {
+public interface RoleRepository extends JpaRepository<RoleEntity, UUID> {
     Optional<RoleEntity> findByName(String name);
 }
