8940046b6fefa2658f1d1fb787bf5f655bcc4182
Nariman <narimansayilov@gmail.com>
2024-08-11 12:19:12 +0400

    change amazon s3 service structure

--- src/main/java/com/storyshare/service/AmazonS3Service.java
+++ src/main/java/com/storyshare/service/AmazonS3Service.java
@@ -25,13 +25,11 @@ public class AmazonS3Service {
     private String bucketName;
 
     public String uploadFile(MultipartFile file) {
-        log.info("ActionLog.uploadFile.start for filename is {}", file.getOriginalFilename());
         File fileObj = convertMultiPartFileToFile(file);
         String fileName = System.currentTimeMillis() + "_" + file.getOriginalFilename();
         amazonS3.putObject(new PutObjectRequest(bucketName, fileName, fileObj)
                 .withCannedAcl(CannedAccessControlList.PublicRead));
         boolean delete = fileObj.delete();
-        log.info("ActionLog.uploadFile.end for filename is {}", file.getOriginalFilename());
         return amazonS3.getUrl(bucketName, fileName).toString();
     }
 
