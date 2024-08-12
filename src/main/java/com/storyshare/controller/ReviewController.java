d1aac7bcf0078ca4ef0f254ad5e41cc9427109ca
00alimardan <00alimardan@gmail.com>
2024-08-12 23:19:29 +0400

    add delete into review

--- src/main/java/com/storyshare/controller/ReviewController.java
+++ src/main/java/com/storyshare/controller/ReviewController.java
@@ -4,11 +4,7 @@ package com.storyshare.controller;
 import com.storyshare.dto.request.ReviewRequest;
 import com.storyshare.dto.response.ReviewResponse;
 import com.storyshare.entity.ReviewEntity;
-import com.storyshare.entity.StoryEntity;
-import com.storyshare.entity.UserEntity;
 import com.storyshare.exception.NotFoundException;
-import com.storyshare.mapper.ReviewMapper;
-import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -37,5 +33,10 @@ public class ReviewController {
     @GetMapping("/replay/{id}")
     public List<ReviewResponse> getReviewReplayById(@PathVariable UUID id) {
         return reviewService.getReviewReplayById(id);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public void deleteReviewById(@PathVariable UUID id) {
+        reviewService.deleteReviewById(id);
     }
 }    
