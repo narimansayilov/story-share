4e986fc0ff707d72f02c561e16760ead4e22c218
Nariman <narimansayilov@gmail.com>
2024-08-12 22:56:51 +0400

    add new implementations

--- src/main/java/com/storyshare/repository/StoryRepository.java
+++ src/main/java/com/storyshare/repository/StoryRepository.java
@@ -2,8 +2,10 @@ package com.storyshare.repository;
 
 import com.storyshare.entity.StoryEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 import java.util.UUID;
 
-public interface StoryRepository extends JpaRepository<StoryEntity, UUID> {
+public interface StoryRepository extends JpaRepository<StoryEntity, UUID>, PagingAndSortingRepository<StoryEntity, UUID>, JpaSpecificationExecutor<StoryEntity> {
 }
