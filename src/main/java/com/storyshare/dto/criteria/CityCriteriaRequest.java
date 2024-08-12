4472a1139c7d3f054b3fc54ac5a50700f7d01ca8
Nariman <narimansayilov@gmail.com>
2024-08-12 09:13:29 +0400

    add city criteria request

---
+++ src/main/java/com/storyshare/dto/criteria/CityCriteriaRequest.java
@@ -0,0 +1,11 @@
+package com.storyshare.dto.criteria;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CityCriteriaRequest {
+    private String name;
+    private Boolean parent;
+}
