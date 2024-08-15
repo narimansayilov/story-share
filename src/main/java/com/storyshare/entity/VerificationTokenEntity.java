7e4a873c37800144edb2ac631e5322ee579dcfad
Nariman <narimansayilov@gmail.com>
2024-08-15 17:26:20 +0400

    add new entity

--- src/main/java/com/storyshare/entity/VerificationTokenEntity.java
+++ src/main/java/com/storyshare/entity/VerificationTokenEntity.java
@@ -1,28 +1,32 @@
 package com.storyshare.entity;
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Entity
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "email_verification_tokens")
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class VerificationTokenEntity {
-
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
-    private Long id;
+    UUID id;
+    String token;
+    String email;
+    LocalDateTime expirationDate;
 
-    @Column(nullable = false, unique = true)
-    private String token;
+    @CreationTimestamp
+    LocalDateTime createdAt;
 
-    @Column(nullable = false)
-    private String email;
-
-    @Column(nullable = false)
-    private LocalDateTime expirationDate;
-
+    @UpdateTimestamp
+    LocalDateTime updatedAt;
 }
--- src/main/java/com/storyshare/repository/VerificationTokenRepository.java
+++ src/main/java/com/storyshare/repository/VerificationTokenRepository.java
@@ -5,9 +5,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
+import java.util.UUID;
 
 @Repository
-public interface VerificationTokenRepository extends JpaRepository<VerificationTokenEntity, Long> {
+public interface VerificationTokenRepository extends JpaRepository<VerificationTokenEntity, UUID> {
 
     Optional<VerificationTokenEntity> findByToken(String token);
 
